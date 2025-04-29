3caaaac0f9f5d04ca1c462a976b7728cba7c6165
Minha Saxena <154838345+minha-saxena@users.noreply.github.com>
2025-04-29 16:50:54 +0530

    firebase.js

--- ChatApp/src/firebase.js
+++ ChatApp/src/firebase.js
@@ -2,21 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Firebase configuration object (your credentials)
 const firebaseConfig = {
-  apiKey: "AIzaSyCDXZnK3KrFp3UcqznsenkdjzXp34pCP_w",
-  authDomain: "chat-app-4bce8.firebaseapp.com",
-  projectId: "chat-app-4bce8",
-  storageBucket: "chat-app-4bce8.firebasestorage.app",
-  messagingSenderId: "87460476412",
-  appId: "1:87460476412:web:5040bc03bbcb8df608ae79",
+  // add your firebase credentials
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and Firestore
-export const auth = getAuth(app);  // Use the `app` instance here for proper initialization
-export const db = getFirestore(app); // If you're using Firestore, initialize it with `app` too
+export const auth = getAuth(app); 
+export const db = getFirestore(app);
 
-export default app; // Export app if needed in other parts of the app
+export default app; 
